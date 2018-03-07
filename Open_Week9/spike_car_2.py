1022b463a88057bed498de2f14047d3bea38b1d3
Student <student@2024-01-203-12.aii.stu>
2018-03-06 18:48:47 -0800

    updates

--- Open_Week9/spike_car_2.py
+++ Open_Week9/spike_car_2.py
@@ -1,16 +1,14 @@
 import maya.cmds;
 def addSpikes(obj):
-    try: polycount = maya.cmds.polyEvaluate(obj, face=True);
-    except: raise;
+    """This function adds spikes to a polygon object."""
+    try: polycount = maya.cmds.polyEvaluate(obj, face=True)
+    except: raise
     for i in range(0, polycount):
-        face = '%s.f[%s]'%(obj, i);
-        maya.cmds.polyExtrudeFacet(face, ltz=1, ch=0);
-        maya.cmds.polyExtrudeFacet(
-            face, ltz=1, ch=0,
-            ls=[0.1, 0.1, 0.1]
-        );
-    maya.cmds.polySoftEdge(obj, a=180, ch=0);
-    maya.cmds.select(obj);
+        face = '%s.f[%s]'%(obj, i)
+        maya.cmds.polyExtrudeFacet(face, ltz=1, ch=0)
+        maya.cmds.polyExtrudeFacet(face, ltz=1, ch=0, ls=[.1,.1,.1])
+    maya.cmds.polySoftEdge(obj, a=180, ch=0)
+    maya.cmds.select(obj)
     
     
 
